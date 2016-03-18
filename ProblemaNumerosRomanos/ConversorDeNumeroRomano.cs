4e8873d17a38885f04b08c371d4afe5751201c9d
Hiran Tassinari <hiran.tassinari@gmail.com>
2016-03-18 14:21:35 -0300

    Adicionando o projeto e o testProject
    Adicionando o caso de test basico junto com o inicio da implementação do projeto de numeros romanos

---
+++ ProblemaNumerosRomanos/ConversorDeNumeroRomano.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProblemaNumerosRomanosTest
+{
+    public class ConversorDeNumeroRomano
+    {
+
+        private static Dictionary<string, int> tabela = new Dictionary<string, int>()
+        {
+            {"I",1 },
+            {"V",5 },
+            {"X",10 },
+            {"L",50 },
+            {"C",100 },
+            {"D",500 },
+            {"M",1000 },
+        };
+
+        public int Converte(string nRomano)
+        {
+            return tabela[nRomano];
+        }
+    }
+}
---
+++ ProblemaNumerosRomanosTest/ConversorDeNumeroRomanoTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProblemaNumerosRomanosTest
+{
+    [TestClass]
+    public class ConversorDeNumeroRomanoTest
+    {
+        [TestMethod]
+        public void DeveEntenderOSimboloI()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("I");
+            Assert.Equals(1, numero);
+        }
+
+        [TestMethod]
+        public void DeveEntenderOSimboloV()
+        {
+            ConversorDeNumeroRomano romano = new ConversorDeNumeroRomano();
+            int numero = romano.Converte("V");
+            Assert.AreEqual(5, numero);
+        }
+    }
+}
